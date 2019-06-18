7d32be9196fcf20f3555a2ac7c9d1297f86beaa2
guoke <523510911@qq.com>
2019-06-18 21:00:16 +0800

    初步配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from 'vue-router'
+import iview from 'iview'
+
+import 'normalize.css'
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+Vue.use(router)
+Vue.use(iview)
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    data:{
+        eventHub:new Vue()
+    }
 }).$mount('#app')
