2742cc27e5761bd975735092cfb5df243807cb5c
MaLeTNT <1069433047@qq.com>
2019-06-19 09:08:29 +0800

    merge guoke

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+Vue.use(iView)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
