bf8d0d2aba54255ebbbb3c888cbf6753252103d8
MaLeTNT <1069433047@qq.com>
2019-06-20 20:30:58 +0800

    6-20 最后一次提交

--- src/components/ASmanage.vue
+++ src/components/ASmanage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="box">
         <!-- 输入框 -->
-        <i-form :model="formItem" :label-width="90" label-position="left" id="form">
+        <i-form :model="formItem"  label-position="left" id="form">
             <Row type="flex">
                 <i-col span="6" order="1" id="input1">
                     <Form-item label="AS号">
@@ -17,7 +17,6 @@
                     <Form-item label="使用状态">
                         <i-select :model.sync="formItem.select" v-model="formItem.status" class="pinput" >
                             <i-option :value="item.value" v-for="item in formItem.statusList" :key="item.value">{{item.label}}</i-option>
-
                         </i-select>
                     </Form-item>
                 </i-col>
@@ -29,12 +28,13 @@
                 </i-col>
             </Row>
         </i-form>
+          <Divider />
         <!-- 中部按钮 -->
         <Row>
             <Col span="18">
-               <Button type="primary" class="btn"><Icon type="ios-list-box-outline"  @click="modal1 = true" />新增</Button>
-               <Button type="primary" class="btn"><Icon type="ios-create-outline" />修改</Button>
-               <Button type="primary" class="btn"><Icon type="ios-trash-outline"/>删除</Button>
+                <Button type="primary" class="btn"  @click="modal1 = true" ><img src="../assets/icon/icon_add.svg" alt="">新增</Button>
+                <Button type="primary" class="btn"><img src="../assets/icon/icon_edit.svg" alt="">修改</Button>
+                <Button type="primary" class="btn"><img src="../assets/icon/icon_delete.svg" alt="">删除</Button>
             </Col>
             <Col span="6" class="btn2">
                 <Button type="primary" > <Icon type="ios-open-outline" />导出</Button>
@@ -52,54 +52,19 @@
             </p>
             <div style="text-align:center">
                 <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80" label-position="right">
-                    <Row>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span="12">
-                            <FormItem label="Name" prop="name">
-                                <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
-                            </FormItem>
-                        </Col>
-                    </Row>
+                    <div class="formBar">
+                        <FormItem label="Name" prop="name">
+                            <Input v-model="formValidate.name" placeholder="Enter your name" />
+                        </FormItem>
+                        <FormItem label="Name" prop="name" class="rBtn">
+                            <Input v-model="formValidate.name" placeholder="Enter your name" class="minput" />
+                        </FormItem>
+                    </div>
                 </Form>
             </div>
-            <div slot="footer">
-                <Button type="error" size="large" long >Delete</Button>
-                <Button type="error" size="large" long >Delete</Button>
+            <div slot="footer" class="fbtn">
+                <Button type="primary">提交</Button>
+                <Button type="primary">退出</Button>
             </div>
         </Modal>
     </div>
@@ -128,7 +93,7 @@
                     {
                         title: '序',
                         key: 'name',
-                        width: 36,
+                        width: 60,
                         fixed: 'left',
                         size:12
                     },
@@ -142,17 +107,17 @@
                     {
                         title: 'AS名称',
                         key: 'province',
-                        width: 100
+                        width: 150
                     },
                     {
                         title: '省份',
                         key: 'city',
-                        width: 100
+                        width: 150
                     },
                     {
                         title: '城市数目',
                         key: 'address',
-                        width: 200
+                        width: 250
                     },
                     {
                         title: 'AS数量',
@@ -162,12 +127,12 @@
                     {
                         title: '用途描述',
                         key: 'describe',
-                        width: 100
+                        width: 200
                     },
                     {
                         title: '是否公有',
                         key: 'zip',
-                        width: 100
+                        width: 200
                     },
                     {
                         title: '备注',
@@ -322,11 +287,9 @@
         background:rgba(255,255,255,1);
         border-radius:3px;
         border:1px solid rgba(215,221,229,1);
+        margin-left:10px;
     }
-    .pbtn{
-        border-radius:4px;
 
-    }
     .modal1{
         height: 298px;
     }
@@ -338,18 +301,13 @@
     }
 
 }
-.minput{
-        width:200px;
-        height:28px;
-        background:rgba(255,255,255,1);
-        border-radius:3px;
-        border:1px solid rgba(215,221,229,1);
-    }
+
  #form{
      font-size:12px;
  }
   #search{
       margin-top: 2px;
+      font-size:12px;
       text-align: right;
       float: left;
         /* margin: 0 auto; */
@@ -358,15 +316,44 @@
             button:first-child{
                 margin-right: 10px;
             }
+            button:last-child{
+                background: #fff;
+                color: rgba(66,133,244,1);
+            }
+             /deep/ span{
+                    margin-left:0;
+                }
+             .pbtn{
+                width:62px;
+                background:rgba(66,133,244,1);
+                border-radius:4px;
+                border:1px solid rgba(66,133,244,1);
+                font-size: 12px;
+                padding: 4px 0;
+            }
+
+
         }
   }
   #btn1{
       margin-right: 20px;
-
   }
-  .btn{
-      margin-right: 1%;
-      font-size: 14px;
+    .btn{
+      width: 88px;
+    margin-right: 10px;
+    font-size: 14px;
+    img{
+        vertical-align: text-top;
+        width: 20px;
+        height: 20px;
+    }
+    &:first-child img{
+        width: 14px;
+        height: 14px;
+        margin-right: 4px;
+        vertical-align: middle;
+    }
+
   }
   .btn2{
         text-align: right;
@@ -379,6 +366,15 @@
  #page{
      margin: 20px;
      float: right;
+ }
+ .fbtn{
+     text-align: center;
+ }
+ .formBar{
+     display: flex;
+     .rBtn{
+        margin-right: 10px;
+     }
  }
 
 </style>
