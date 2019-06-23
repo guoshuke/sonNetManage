98d9b56066fe06446bdee479106a9def0e203781
MaLeTNT <1069433047@qq.com>
2019-06-23 18:12:12 +0800

    6-23 commit

---
+++ src/components/ASAddModal.vue
@@ -0,0 +1,163 @@
+<!--  -->
+<template>
+  <div>
+      <!-- 新增弹出框 -->
+        <Modal v-model="modal1" width="750" class="addModal">
+            <p slot="header" style="color:#fff;">
+              <span>新增</span>
+            </p>
+            <div style="text-align:center" class="middleInput">
+                <Form ref="formValidate" :model="formData" :rules="ruleValidate" :label-width="80" label-position="right">
+                    <div class="formBar">
+                        <Form-item  label="省份" prop="province">
+                            <Select v-model="formData.province" class="minput">
+                              <Option :value="item.value" v-for="item in formData.provinceList" :key="item.value">{{item.label}}</Option>
+                            </Select>
+                        </Form-item>
+                        <FormItem label="城市数目" prop="PrNumber" class="rBtn">
+                            <Input v-model="formData.PrNumber" placeholder="输入文字" class="minput" />
+                        </FormItem>
+                    </div>
+                    <div class="formBar">
+                        <Form-item  label="AS数量" prop="ASNumber">
+                          <Input v-model="formData.ASNumber" class="minput" />
+                        </Form-item>
+                        <FormItem label="AS名称" prop="ASName" class="rBtn">
+                          <Input v-model="formData.ASName" placeholder="输入文字" class="minput" />
+                        </FormItem>
+                    </div>
+                    <div class="formBar">
+                        <Form-item  label="用途描述" prop="useDescription" >
+                          <Input v-model="formData.useDescription" class="minput" placeholder="输入文字" />
+                        </Form-item>
+                        <Form-item  label="是否公有" prop="isPublic" class="rBtn">
+                            <Select v-model="formData.isPublic" class="minput">
+                              <Option :value="item.value" v-for="item in formData.isPublicList" :key="item.value">{{item.label}}</Option>
+                            </Select>
+                        </Form-item>
+                    </div>
+                    <div class="formBar">
+                        <Form-item  label="备注" prop="">
+                          <Input v-model="formData.remark" class="minput" placeholder="输入文字" />
+                        </Form-item>
+                    </div>
+                </Form>
+            </div>
+            <div slot="footer" class="fbtn">
+              <Button @click="handleEsc" class="fpbtn">取消</Button>
+              <Button type="primary" class="fpbtn">提交</Button>
+            </div>
+        </Modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+          modal1: false,
+          formData: {
+              province:'',
+              provinceList:[
+                {
+                  label:'河南',
+                  value:'河南'
+                },
+                {
+                  label:'山西',
+                  value:'山西'
+                }
+              ],
+              PrNumber: '',
+              ASNumber: 2,
+              ASName:'',
+              useDescription:'',
+              isPublic:'',
+              isPublicList:[
+                {label:'是',value:'是'},
+                {label:'否',value:'否'}
+              ],
+               remark:''
+          },
+          ruleValidate: {
+              province: [
+                  { required: true, trigger: 'blur' }
+              ],
+              PrNumber: [
+                  { required: true, message: '请输入城市数目', trigger: 'blur' },
+              ],
+              ASNumber: [
+                  { required: true, message: '请输入AS数量', trigger: 'blur' },
+              ],
+              ASName: [
+                  { required: true, message: '请输入AS名称', trigger: 'blur' },
+              ],
+             useDescription: [
+                  { required: true, message: '请输入您的用途描述', trigger: 'blur' },
+              ],
+             isPublic: [
+                  { required: true, message: '请选择', trigger: 'blur' },
+              ],
+          }
+      }
+    },
+  methods: {
+    // 取消按钮
+    handleEsc() {
+      this.modal1 = false
+    }
+  },
+  mounted () {
+
+    this.formData.province = this.formData.provinceList[0].value
+    this.formData.isPublic = this.formData.isPublicList[0].value
+  }
+}
+
+</script>
+<style lang='less' scoped>
+    //  新增弹出框样式
+.addModal{
+    width: 750px;
+    height: 298px;
+    /deep/.ivu-modal-header {
+    width:750px;
+    height:50px;
+    background:rgba(66,133,244,1);
+    border-radius:0px 0px 4px 4px;
+
+  }
+  /deep/.ivu-modal-close .ivu-icon-ios-close{
+        color:#fff;
+    }
+  /deep/.ivu-modal-body{
+    margin-top:18px;
+    padding:  0 28px 0 77px;
+    box-sizing: border-box;
+    }
+     .middleInput{
+      height: 200px;
+      /deep/.ivu-form-item {
+        margin-bottom: 18px;
+      }
+    }
+}
+.minput{
+  width: 200px;
+  text-align: left;
+}
+ .fbtn{
+    text-align: center;
+     .fpbtn{
+      width: 86px;
+      height: 36px;
+    }
+ }
+ .formBar{
+    display: flex;
+
+}
+.rBtn{
+    margin-left: 90px;
+}
+</style>
---
+++ src/components/ipBusImport.vue
@@ -0,0 +1,118 @@
+<!-- ip业务导入 -->
+<template>
+  <div>
+     <!-- 顶部 -->
+    <span class="top">上传文件</span>
+    <!-- 中间卡片 -->
+    <Card style="width:100%;margin-top:13px;height:113px">
+      <div style="margin-bottom:20px">
+        <span style="font-size:12px;margin-right:24px">导入类型</span>
+        <Radio-group v-model="imporType">
+          <Radio label="二级规划"></Radio>
+          <Radio label="三级规划"></Radio>
+          <Radio label="分配未启用"></Radio>
+          <Radio label="集客已启动"></Radio>
+          <Radio label="非集客已启动"></Radio>
+        </Radio-group>
+      </div>
+      <div>
+        <span style="font-size:12px;margin-right:24px">上传文件</span>
+        <input type="text" class="fileInput">
+        <Button class="btns">选择</Button>
+        <Button type="primary" class="priBtn" @click="showResult">下一步</Button>
+        <Button class="btns">规划模板</Button>
+        <Button class="btns">分配未启用模板</Button>
+        <Button class="btns">集客一起用模板</Button>
+        <Button class="btns">非集客一起用模板</Button>
+        <Button class="btns">小红书</Button>
+      </div>
+    </Card>
+    <!-- 底部信息 -->
+    <div style="margin-top:20px" v-if="isShow">
+      <!-- 头部 -->
+      <span class="footer" style="margin-left:10px">数据检验报告</span>
+      <Button class="btns" style="float:right">清空</Button>
+       <!-- 文件下载 -->
+       <Card style="width:100%;margin-top:13px;height:86px">
+         <p style="color:rgba(66,133,244,1);font-size:14px; margin-bottom:8px">点击下载结果文件</p>
+         <p>共识别出6行数据需要导入/其中6行数据未通过校验/0行数据导入成功/</p>
+       </Card>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      importType:'',
+      // 显示结果
+      isShow: false
+    }
+  },
+
+  methods: {
+    showResult() {
+      this.isShow = true
+    }
+  }
+}
+
+</script>
+<style lang='less' scoped>
+.top{
+  margin-left: 10px;
+}
+.top::after{
+  content:"";
+  width: 4px;
+  height: 10px;
+  display: block;
+  height:10px;
+  background:rgba(66,133,244,1);
+  border-radius:2px;
+  position: absolute;
+  top:29px;
+}
+.ivu-radio-wrapper{
+  margin-right: 16px;
+}
+.btns{
+  height:28px;
+  background:rgba(255,255,255,1);
+  border-radius:3px;
+  border:1px solid rgba(66,133,244,1);
+  line-height: 19px;
+  margin-right: 10px;
+  color:rgba(66,133,244,1);
+}
+.priBtn{
+    height:28px;
+  border-radius:3px;
+  line-height: 19px;
+  margin-right: 30px;
+
+}
+.fileInput{
+  width:300px;
+  height:28px;
+  background:rgba(255,255,255,1);
+  border-radius:4px;
+  border:1px solid rgba(229,229,229,1);
+  margin-right: 5px;
+  margin-top: 3px;
+}
+.footer::after{
+  content:"";
+  width: 4px;
+  height: 10px;
+  display: block;
+  height:10px;
+  background:rgba(66,133,244,1);
+  border-radius:2px;
+  position: absolute;
+  top:194px;
+}
+</style>
